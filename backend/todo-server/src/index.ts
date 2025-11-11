c4c55a0bfe5435b0de0123fcbb3697eb235ca3b3
cyrus-nodejs <adeyemiemma45@gmail.com>
2025-11-11 20:35:37 +0100

    feat: Add node-cron

--- backend/todo-server/src/index.ts
+++ backend/todo-server/src/index.ts
@@ -1,4 +1,7 @@
 import dotenv from 'dotenv';
+
+import cron from 'node-cron';
+import axios from 'axios';
 dotenv.config();
 
 import app from './server';
@@ -6,6 +9,17 @@ import { connectDB } from './config/db';
 
 
 const PORT = process.env.PORT || 4000;
+
+
+cron.schedule('*/1440 * * * *', async () => {
+  try {
+    const url = process.env.SERVER_URL!;
+    await axios.get(url);
+    console.log('Ping sent to:', url);
+  } catch (err) {
+    console.error('Ping failed', err);
+  }
+});
 
 (async () => {
   try {
