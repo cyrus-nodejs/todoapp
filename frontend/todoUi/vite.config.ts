040a96b85d1e22383e75f799493179771eb68744
cyrus-nodejs <adeyemiemma45@gmail.com>
2025-11-13 23:35:40 +0100

    feat: .end config

--- frontend/todoUi/vite.config.ts
+++ frontend/todoUi/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
    define: {
-    __API_ORIGIN__: JSON.stringify(process.env.VITE_API_BASE_URL || "https://todoapp-cd8m.onrender.com"),
+    __API_ORIGIN__: JSON.stringify(process.env.VITE_API_BASE_SERVER || "https://todoapp-cd8m.onrender.com"),
   },
   plugins: [react()],
 })
